5256f776979803e3f72a7904eba28cdfb9f74d68
Anthony Vanegas <avanegas04@gmail.com>
2018-12-02 22:02:27 -0500

    express added to app.js

--- futbolLegends/app.js
+++ futbolLegends/app.js
@@ -1,4 +1,15 @@
-console.log ("running app.js");
 
 const express = require('express');
+var app = express();
+
 const fs = require('fs');
+
+app.get('/players', (req, res) => {
+    //res.send('Hello World');
+    var players = fs.readFileSync('players.json');
+    players = JSON.parse(players);
+    res.send(players);
+});
+
+// listen on port 3000
+app.listen(3000);
